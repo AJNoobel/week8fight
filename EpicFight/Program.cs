a79c045954b02bdf4258a0aabb658a8054a388df
Opilane TTHK <opilane@tthk.ee>
2021-10-20 11:38:19 +0300

    yes

---
+++ EpicFight/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace EpicFight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hero = PickHero();
+            string villain = PickVillain();
+            Console.WriteLine($"{hero} will fight {villain}.");
+
+            string heroWeapon = PickWeapon();
+            string villianWeapon = PickWeapon();
+            Console.WriteLine($"{hero} picked {heroWeapon}. {villian} picked {villianWeapon}");
+        }
+
+        private static string PickHero()
+        {
+            string[] superHeroes = { "OsamaBinLaden", "Batman", "Spiderman", "SpongeBobCrackPants", "OptimusBlyatuimus" };
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, superHeroes.Length);
+
+            return superHeroes[randomIndex];
+        }
+
+        private static string PickVillain()
+        {
+            string[] superVillians = { "Joker", "JohnnySins", "PiiderMan", "BarrackOsama", "MiaKhalifa" };
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, superVillians.Length);
+
+            return superVillians[randomIndex];
+
+
+
+        }
+        private static string pickWeapon()
+        {
+            string[] Weapon = { "OsamaBinLaden", "Batman", "Spiderman", "SpongeBobCrackPants", "OptimusBlyatuimus" };
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, Weapon.Length);
+
+            return Weapon[randomIndex];
+        }
+            
+
+    }
+}
---
+++ FunctionsWithArrays/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace FunctionsWithArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] colors = { "red", "blue", "green" };
+            string randomColor = PickRandomColor(colors);
+            DisplayRandomColor(colors);
+            Console.WriteLine($"arvuti valis {randomColor}.");
+     
+        
+        }
+
+        private static int GenerateRandomNumberForArray(string[] someArray)
+        {
+            Random rnd = new Random();
+            return rnd.Next(0, someArray.Length);
+        }
+
+
+
+
+
+        private static string PickRandomColor(string[] someArray)
+        {
+
+        
+            return someArray[GenerateRandomNumberForArray(someArray)];
+
+        }
+
+
+
+
+        private static void DisplayRandomColor(string[] someArray)
+        {
+            
+                Console.WriteLine(someArray [GenerateRandomNumberForArray(someArray)]);
+
+        }
+
+
+
+
+
+
+
+    }
+}
